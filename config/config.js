dbdb1d757678d99eaa8c5a5b462a9d2bd9c97093
Osman Pérez <osmanperez66@gmail.com>
2023-12-18 07:18:57 -0600

    [FIX] Manejo de errores

--- config/config.js
+++ config/config.js
@@ -3,4 +3,19 @@ require('dotenv').config();
 const port = process.env.PORT
 const clientKey = process.env.CLIENT_KEY
 
-module.exports = { port, clientKey }
+// Function convert image url to base64
+async function imageUrlToBase64(url) {
+    try {
+        const response = await fetch(url);
+        const blob = await response.arrayBuffer();
+        const contentType = response.headers.get('content-type');
+        const base64String = `data:${contentType};base64,${Buffer.from(
+            blob,
+        ).toString('base64')}`;
+        return base64String;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = { port, clientKey, imageUrlToBase64 }
--- controllers/stealthMode.controller.js
+++ controllers/stealthMode.controller.js
@@ -10,105 +10,79 @@ const clientKey = config.clientKey
 exports.stealthMode = async (req, res) => {
     const { url, options } = req.body;
     await chromium.launch(options).then(async browser => {
-        try {
-            const page = await browser.newPage();
-            page.setDefaultTimeout(3600000);
-            await page.goto(url);
-            const result = await page.innerHTML('*', { waitUntil: 'networkidle' });
-            await page.close();
-            await browser.close();
-            res.send("<html>" + result.replace(new RegExp("\\n", 'g'), "") + "</html>");
-        } catch (error) {
-            res.send({ status: 500, message: error });
-        } finally {
-            await browser.close();
-        }
+        const page = await browser.newPage();
+        page.setDefaultTimeout(3600000);
+        await page.goto(url);
+        const result = await page.innerHTML('*', { waitUntil: 'networkidle' });
+        await page.close();
+        await browser.close();
+        res.send("<html>" + result.replace(new RegExp("\\n", 'g'), "") + "</html>");
     }).catch(error => {
         res.send({ status: 500, message: error });
     });
 }
 
 exports.stealthMode2CaptchaAmazon = async (req, res) => {
-    const { url, options} = req.body;
+    const { url, options } = req.body;
 
     await chromium.launch(options).then(async browser => {
-        try {
-            const page = await browser.newPage();
-            page.setDefaultTimeout(3600000);
-            await page.goto(url);
-            const existBlock = await page.$('input#captchacharacters');
-            if (existBlock != null) {
 
-                var task = "";
-                var text = "";
+        const page = await browser.newPage();
+        page.setDefaultTimeout(3600000);
+        await page.goto(url);
+        const existBlock = await page.$('input#captchacharacters');
+        if (existBlock != null) {
 
-                let image = await page.locator('.a-row img');
-                let imageSrc = await image.getAttribute('src');
-                let body = await imageUrlToBase64(imageSrc);
+            var task = "";
+            var text = "";
 
-                await axios.post('https://api.2captcha.com/createTask', {
-                    "clientKey": clientKey,
-                    "task": {
-                        "type": "ImageToTextTask",
-                        "body": body
-                    },
-                }).then(function (response) {
-                    console.log(response.data)
-                    if (response.data.taskId != null) {
-                        task = response.data.taskId;
-                    }
-                })
+            let image = await page.locator('.a-row img');
+            let imageSrc = await image.getAttribute('src');
+            let body = await config.imageUrlToBase64(imageSrc);
 
-                if (task != "") {
-                    let solve_Ready = true;
-                    do {
-                        await axios.post('https://api.2captcha.com/getTaskResult', {
-                            "clientKey": clientKey,
-                            "taskId": task
-                        }).then(function (response) {
-                            console.log(response.data)
-                            if (response.data.status != "processing") {
-                                solve_Ready = false;
-                                if (response.data.solution != null) {
-                                    text = response.data.solution.text;
-                                }
+            await axios.post('https://api.2captcha.com/createTask', {
+                "clientKey": clientKey,
+                "task": {
+                    "type": "ImageToTextTask",
+                    "body": body
+                },
+            }).then(function (response) {
+                console.log(response.data)
+                if (response.data.taskId != null) {
+                    task = response.data.taskId;
+                }
+            })
+
+            if (task != "") {
+                let solve_Ready = true;
+                do {
+                    await axios.post('https://api.2captcha.com/getTaskResult', {
+                        "clientKey": clientKey,
+                        "taskId": task
+                    }).then(function (response) {
+                        console.log(response.data)
+                        if (response.data.status != "processing") {
+                            solve_Ready = false;
+                            if (response.data.solution != null) {
+                                text = response.data.solution.text;
                             }
-                        })
-                    } while (solve_Ready);
+                        }
+                    })
+                } while (solve_Ready);
 
-                    if (text != "") {
-                        await page.locator('input#captchacharacters').fill(text);
-                        sleep(5000);
-                        await page.click(`button[type="submit"]`)
-                    }
+                if (text != "") {
+                    await page.locator('input#captchacharacters').fill(text);
+                    sleep(5000);
+                    await page.click(`button[type="submit"]`)
                 }
             }
-            const result = await page.innerHTML('*', { waitUntil: 'networkidle' });
-            await page.close();
-            await browser.close();
-            res.send("<html>" + result.replace(new RegExp("\\n", 'g'), "") + "</html>");
-        } catch (error) {
-            res.send({ status: 500, message: error });
-        } finally {
-            await browser.close();
         }
+        const result = await page.innerHTML('*', { waitUntil: 'networkidle' });
+        await page.close();
+        await browser.close();
+        res.send("<html>" + result.replace(new RegExp("\\n", 'g'), "") + "</html>").status(200);
+
     }).catch(error => {
-        res.send({ status: 500, message: error });
+        res.send({ message: error }).status(500);
     });
-}
-
-
-// Function convert image url to base64
-async function imageUrlToBase64(url) {
-    try {
-        const response = await fetch(url);
-        const blob = await response.arrayBuffer();
-        const contentType = response.headers.get('content-type');
-        const base64String = `data:${contentType};base64,${Buffer.from(
-            blob,
-        ).toString('base64')}`;
-        return base64String;
-    } catch (err) {
-        console.error(err);
-    }
 }
